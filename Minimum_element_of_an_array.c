7320981981a92fb7f7ef97da425549a402583bf6
ManohariMarisetti <147469404+ManohariMarisetti@users.noreply.github.com>
2023-12-06 15:19:49 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int x=arr[0];     for(i=0;i<n;i++)     {         if(x>arr[i])         {             x=arr[i];         }     }     printf("%d",x); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int x=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(x>arr[i])
+        {
+            x=arr[i];
+        }
+    }
+    printf("%d",x);
+}
